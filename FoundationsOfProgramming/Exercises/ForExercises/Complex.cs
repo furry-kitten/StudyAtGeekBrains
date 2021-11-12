b7141d4facd5602c7cdc42618c34b7a3901a2393
v.tokarev <v.tokarev@crmguru.ru>
2021-11-12 22:26:36 +0300

    First exercise done

---
+++ FoundationsOfProgramming/Exercises/ForExercises/Complex.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsOfProgramming.Exercises.ForExercises
+{
+    internal class Complex
+    {
+
+        #region Поля (Fields)
+
+        /// <summary>
+        /// Мнимая часть комплексного числа
+        /// </summary>
+        private double im;
+
+        /// <summary>
+        /// Действительная часть комплексного числа
+        /// </summary>
+        private double re; // private
+
+        #endregion
+
+        #region Свойства
+
+        public double Ip {
+            get => im;
+
+            set {
+                if (value == 0) {
+                    throw new Exception("Недопустимое значение.");
+                }
+
+                im = value;
+            }
+        }
+
+        public double Rp {
+            get => re;
+            set => re = value;
+        }
+
+        #endregion
+
+        #region Конструктор класса
+
+        public Complex(double re, double im) {
+            if (im == 0) {
+                throw new Exception("Недопустимое значение.");
+            }
+
+            this.re = re;
+            this.im = im;
+
+        }
+
+        private Complex() {
+
+        }
+
+        #endregion
+
+        #region Поведение (Methods)
+
+        public Complex Plus(Complex complex) => new Complex(re + complex.Rp, im + complex.Ip);
+
+        public Complex Mines(Complex complex) => new Complex(re - complex.Rp, im - complex.Ip);
+
+        public Complex Multiplication(Complex complex) => new Complex(re * complex.Rp + im * complex.Ip * (-1), re * complex.Ip + im * complex.Rp);
+
+        #endregion
+
+        public override string ToString() => re != 0 ? $"{re} + {im}i" : $"{im}i";
+    }
+}
---
+++ FoundationsOfProgramming/Exercises/Lesson3/Exercise1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BasicForStuding;
+using FoundationsOfProgramming.Exercises.ForExercises;
+
+namespace FoundationsOfProgramming.Exercises.Lesson3
+{
+    public class Exercise1 : BaseExercise
+    {
+        private Complex firstNumber;
+
+        private Complex secondNumber;
+
+        internal Complex Multiple { get; private set; }
+
+        public Exercise1(BaseExercise next) : base(next) { }
+
+        public override string Description { get; set; } = "Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.\r\n" +
+                                                           "а) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.\r\n" +
+                                                           "б) Добавить диалог с использованием switch демонстрирующий работу класса.";
+
+        public override string Name { get; set; } = "Дописать структуру Complex.";
+
+        protected override void ExecuteExercise() {
+            Complex[] complexNumbers = GetComplexNumbers();
+            firstNumber = complexNumbers[0];
+            secondNumber = complexNumbers[1];
+            Multiple = firstNumber.Multiplication(secondNumber);
+        }
+
+        protected override void SetResult() {
+            Result = $"({firstNumber}) * ({secondNumber}) = {Multiple}";
+        }
+
+        private Complex[] GetComplexNumbers() {
+            int realPartOfFirstNumber = GetInt32FromUserDate($"Your real part first {nameof(Complex)} number: ");
+            int imaginaryPartOfFirstNumber = GetInt32FromUserDate($"Your imaginary component first {nameof(Complex)} number: ");
+            int realPartOfSecondNumber = GetInt32FromUserDate($"Your real part second {nameof(Complex)} number: ");
+            int imaginaryPartOfSecondNumber = GetInt32FromUserDate($"Your imaginary component second {nameof(Complex)} number: ");
+
+            return new[] {
+                new Complex(realPartOfFirstNumber, imaginaryPartOfFirstNumber),
+                new Complex(realPartOfSecondNumber, imaginaryPartOfSecondNumber)
+            };
+        }
+    }
+}
