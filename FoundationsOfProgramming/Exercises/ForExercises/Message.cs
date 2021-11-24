f41f3373855baf876b385b3e20efa9eb4434ec20
v.tokarev <v.tokarev@crmguru.ru>
2021-11-25 00:11:57 +0300

    Remove comments

--- FoundationsOfProgramming/Exercises/ForExercises/Message.cs
+++ FoundationsOfProgramming/Exercises/ForExercises/Message.cs
@@ -32,24 +32,11 @@ namespace FoundationsOfProgramming.Exercises.ForExercises
             return words.ToArray();
         }
 
-        //public static string[] GetWordsWithLength(string message, int lettersCount) {
-        //    var strs = new List<string>();
-        //    Regex reg = GetRegularExpression(lettersCount);
-        //    MatchCollection matches = reg.Matches(message);
-
-        //    foreach (Match match in matches) {
-        //        strs.Add(match.Value);
-        //    }
-
-        //    return strs.ToArray();
-        //}
-
         private static Regex GetRegularExpression(int lettersCount) {
             var strBuilder = new StringBuilder(@"\b\w{");
             strBuilder.Append(lettersCount);
             strBuilder.Append(@",}\b");
 
-            //return new Regex(@"\b\w{3,}\b");
             return new Regex(strBuilder.ToString());
         }
 
@@ -58,18 +45,7 @@ namespace FoundationsOfProgramming.Exercises.ForExercises
             strBuilder.Append(symbol);
             strBuilder.Append(@")\b");
 
-            //return new Regex(@"\b\w*(?<!r)\b");
             return new Regex(strBuilder.ToString());
         }
-
-        //private static Regex GetRegularExpressionBiggestWord(int lettersCount) {
-        //    var strBuilder = new StringBuilder(@"\b\w{");
-        //    strBuilder.Append(lettersCount);
-        //    strBuilder.Append(@",} +|\b ?\w{");
-        //    strBuilder.Append(lettersCount);
-        //    strBuilder.Append(@",}\z");
-
-        //    return new Regex(/*strBuilder.ToString()*/@"\b\w{3} +|\b ?\w{3}\za/\w[a-z]{0,}/gi");
-        //}
     }
 }
