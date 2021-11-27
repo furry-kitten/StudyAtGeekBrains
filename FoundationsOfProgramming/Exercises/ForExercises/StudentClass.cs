0184e4607b8ca1836e74e8d16fd30f3c39a774d8
v.tokarev <v.tokarev@crmguru.ru>
2021-11-27 17:23:32 +0300

    Lesson 6 done

---
+++ FoundationsOfProgramming/Exercises/ForExercises/StudentClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationsOfProgramming.Exercises.ForExercises
+{
+    public class StudentClass
+    {
+        public string FirstName;
+
+        public string SecondName;
+
+        public string Univercity;
+
+        public string Faculty;
+
+        public string Department;
+
+        public int Age;
+
+        public int Course;
+
+        public int Group;
+
+        public string City;
+
+        public StudentClass() { }
+
+        public override string ToString() => $"{SecondName}\t{FirstName}\t{Age}\t{Course}";
+    }
+}
---
+++ FoundationsOfProgramming/Exercises/Lesson6/Exercise1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using BasicForStuding;
+
+namespace FoundationsOfProgramming.Exercises.Lesson6
+{
+    public class Exercise1 : BaseExercise
+    {
+        private StringBuilder stringBuilder = new StringBuilder();
+
+        public delegate double Fun(double x);
+
+        public delegate double Fun2(double x, double y);
+
+        public Func<double, double> Func1;
+
+        public Exercise1(BaseExercise next) : base(next) { }
+
+        public override string Description { get; set; } = "Изменить программу вывода таблицы функции так, чтобы можно было передавать функции типа double (double, double). " +
+                                                           "Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).";
+
+        public override string Name { get; set; } = "Изменить делегат.";
+
+        protected override void SetResult() {
+            Result = stringBuilder.ToString();
+            stringBuilder = new StringBuilder();
+        }
+
+        protected override void ExecuteExercise() {
+            double a = GetDoubleFromUserDate("Enter a = ");
+            stringBuilder.AppendLine("Таблица функции MyFunc:");
+            Table(MyFunc, -2, 2, a);
+            stringBuilder.AppendLine("Таблица функции a*x^2:");
+            Table((x, y) => y * Math.Sin(x), 0, 3, a);
+        }
+
+        public void Table(Func<double, double, double> func, double x, double b, double a) {
+            stringBuilder.AppendLine("----- X ----- Y -----");
+            while (x <= b) {
+                stringBuilder.AppendLine($"| {x,8:F3} | {func(x, a),8:f3} |");
+                x++;
+            }
+            stringBuilder.AppendLine("---------------------\r\n");
+        }
+
+        public double MyFunc(double x, double y) {
+            return y * Math.Pow(x, 2);
+        }
+
+    }
+}
